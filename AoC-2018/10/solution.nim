ddaf780d3798183234424ae2254aa4964c30b9eb
anonanonymous <23041331+anonanonymous@users.noreply.github.com>
2018-12-10 19:13:31 -0800

    cleanup

--- AoC-2018/10/solution.nim
+++ AoC-2018/10/solution.nim
@@ -1,23 +1,23 @@
 import re, sequtils, strutils
 
-proc converge(positions, velocities: ref seq[tuple[x, y: int]]): int =
+proc converge(stars: ref seq[tuple[x, y, h, v: int]]): int =
     var prev = high(int)
 
-    while max(positions[])[0] - min(positions[])[0] < prev:
+    while true:
+        prev = max(stars[])[0] - min(stars[])[0]
+        if (max(stars[])[0] + max(stars[])[2]) - (min(stars[])[0] + min(stars[])[2]) > prev:
+            break
+        for i in 0 .. stars[].high:
+            stars[][i][0] = stars[][i][0] + stars[][i][2]
+            stars[][i][1] = stars[][i][1] + stars[][i][3]
         result += 1
-        prev = max(positions[])[0] - min(positions[])[0]
-        for i in 0 .. positions[].high:
-            positions[][i] = (positions[][i][0] + velocities[][i][0], positions[][i][1] + velocities[][i][1])
-    for i in 0 .. positions[].high:
-        positions[][i] = (positions[][i][0] - velocities[][i][0], positions[][i][1] - velocities[][i][1])
-    return result - 1
 
-proc display(positions: seq[tuple[x, y: int]]): seq[seq[char]] =
+proc display(stars: seq[tuple[x, y, h, v: int]]): seq[seq[char]] =
     var min_x, min_y = high(int)
     var max_x, max_y = low(int)
     var grid: seq[seq[char]]
 
-    for i in positions:
+    for i in stars:
         if i[0] < min_x: min_x = i[0]
         if i[1] < min_y: min_y = i[1]
         if i[0] > max_x: max_x = i[0]
@@ -28,18 +28,16 @@ proc display(positions: seq[tuple[x, y: int]]): seq[seq[char]] =
         grid[i] = newSeq[char](max_x - min_x + 1)
         for j in 0 .. grid[i].high: grid[i][j] = '.'
 
-    for i in 0 .. positions.high:
-        grid[positions[i][1] - abs(min_y)][positions[i][0] - abs(min_x)] = '#'
+    for i in 0 .. stars.high:
+        grid[stars[i][1] - abs(min_y)][stars[i][0] - abs(min_x)] = '#'
 
     return grid
 
-var velocities, positions: ref seq[tuple[x, y: int]]
-positions = new seq[tuple[x, y: int]]
-velocities = new seq[tuple[x, y: int]]
+var stars: ref seq[tuple[x, y, h, v: int]] = new seq[tuple[x, y, h, v: int]]
 
 for line in readFile("input.txt").strip.splitLines:
     let tmp = re.findall(line, re"-?\d+")
-    positions[].add((parseInt(tmp[0]), parseInt(tmp[1])))
-    velocities[].add((parseInt(tmp[2]), parseInt(tmp[3])))
-echo "seconds: ", converge(positions, velocities)
-for i in display(positions[]): echo cast[string](i)
+    stars[].add((parseInt(tmp[0]), parseInt(tmp[1]), parseInt(tmp[2]), parseInt(tmp[3])))
+
+echo "seconds: ", converge(stars)
+for i in display(stars[]): echo cast[string](i)
