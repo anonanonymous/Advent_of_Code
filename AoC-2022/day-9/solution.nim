72d093db1844863684307f05f47b620266810c42
anonanonymous <23041331+anonanonymous@users.noreply.github.com>
2022-12-10 00:38:54 -0800

    add pt2, cleanup

--- AoC-2022/day-9/solution.nim
+++ AoC-2022/day-9/solution.nim
@@ -7,68 +7,66 @@ proc getDiff(head: tuple, tail: tuple): tuple =
     let diff = (x: abs(head.x - tail.x), y: abs(head.y - tail.y))
     return diff
 
-proc handleMove(head: var tuple, tail: var tuple, move: seq): CountTable[string] =
-        let steps = parseInt(move[1])
-        case move[0]
-        of "R":
-            for i in 0..<steps:
-                head.x += 1
-                let diff = getDiff(head, tail)
-                echo diff, " ", move[0]
-                if diff.x == 2:
-                    tail.x += 1
-                    tail.y += head.y - tail.y
-                if diff.y == 2:
-                    tail.x += 1
-                    tail.y += head.y - tail.y
-                result.inc($tail.x & $tail.y)
-        of "L":
-            for i in 0..<steps:
-                head.x -= 1
-                let diff = getDiff(head, tail)
-                echo diff, " ", move[0]
-                if diff.x == 2:
-                    tail.x -= 1
-                    tail.y += head.y - tail.y
-                if diff.y == 2:
-                    tail.x -= 1
-                    tail.y += head.y - tail.y
-                result.inc($tail.x & $tail.y)
-        of "U":
-            for i in 0..<steps:
-                head.y += 1
-                let diff = getDiff(head, tail)
-                echo diff, " ", move[0]
-                if diff.x == 2:
-                    tail.y += 1
-                    tail.x += head.x - tail.x
-                if diff.y == 2:
-                    tail.y += 1
-                    tail.x += head.x - tail.x
-                result.inc($tail.x & $tail.y)
-        of "D":
-            for i in 0..<steps:
-                head.y -= 1
-                let diff = getDiff(head, tail)
-                if diff.x == 2:
-                    tail.y -= 1
-                    tail.x += head.x - tail.x
-                if diff.y == 2:
-                    tail.y -= 1
-                    tail.x += head.x - tail.x
-                result.inc($tail.x & $tail.y)
-        return result
+proc handleMove(head: var tuple, tail: var tuple): tuple =
+    result = (x: 0, y: 0)
+    let diff = getDiff(head, tail)
+    if diff.x > 1 and diff.y == 0:
+        if head.x > tail.x:
+            result.x += 1
+        else:
+            result.x -= 1
+    elif diff.y > 1 and diff.x == 0:
+        if head.y > tail.y:
+            result.y += 1
+        else:
+            result.y -= 1
+    elif (diff.x == 2 and diff.y == 2) or (diff.x == 2 and diff.y == 1) or (diff.y == 2 and diff.x == 1):
+        if head.y > tail.y:
+            result.y += 1
+        else:
+            result.y -= 1
+        if head.x > tail.x:
+            result.x += 1
+        else:
+            result.x -= 1
+    return result
 
-
-proc partOne(input: seq[string]): void =
-    var head = (x: 0, y: 0)
-    var tail = (x: 0, y: 0)
+proc simulateMovement(ropeSize: int, input: seq[string]): void =
+    var rope: seq[tuple[x: int, y: int]]
+    for i in 0..<ropeSize: rope.add((x: 0, y: 0))
     var tailPositions = initCountTable[string]()
 
     for i in input:
         let move = i.split(" ")
-        tailPositions.merge(handleMove(head, tail, move))
+        let steps = parseInt(move[1])
+        case move[0]
+        of "R", "L":
+            for step in 0..<steps:
+                rope[0].x += (if move[0] == "R": 1 else: -1)
+                for j in 1..rope.high:
+                    let diff = handleMove(rope[j-1], rope[j])
+                    rope[j].x += diff.x
+                    rope[j].y += diff.y
+                    if j == rope.high:
+                        tailPositions.inc($rope[j].x & $rope[j].y)
+        of "U", "D":
+            for i in 0..<steps:
+                rope[0].y += (if move[0] == "U": 1 else: -1)
+                for j in 1..rope.high:
+                    let diff = handleMove(rope[j-1], rope[j])
+                    rope[j].x += diff.x
+                    rope[j].y += diff.y
+                    if j == rope.high:
+                        tailPositions.inc($rope[j].x & $rope[j].y)
+
     echo len(tailPositions)
+
+proc partOne(input: seq[string]): void =
+    simulateMovement(2, input)
+
+proc partTwo(input: seq[string]): void =
+    simulateMovement(10, input)
 
 let f = split(strip(readFile("input.txt")), "\n")
 partOne(f)
+partTwo(f)
